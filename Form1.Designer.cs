19dc809c1e401a3d6f8be42ed5dd581ef00ce019
H. Yusuf Usta <46392667+Quiec@users.noreply.github.com>
2019-09-19 11:28:48 +0300

    first release

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace IPTVCHECKER_C_SHARP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 401);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(639, 156);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "UserList";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 578);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(639, 156);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "PassList";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(639, 118);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Crack!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 182);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(639, 213);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(639, 31);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "http://iptv:8080";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 737);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(615, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Algoritma Sitenizin Testi İçin Yusuf Usta Tarafından Yapılmıştır.";
+            // 
+            // button3
+            // 
+            this.button3.Image = global::IPTVCHECKER_C_SHARP.Properties.Resources.github_logo_media_network_social_icon_1320193179934760836_128;
+            this.button3.Location = new System.Drawing.Point(371, 777);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(256, 256);
+            this.button3.TabIndex = 7;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::IPTVCHECKER_C_SHARP.Properties.Resources.bot_chat_message_telegram_icon_1320168256174387607_128;
+            this.button2.Location = new System.Drawing.Point(12, 777);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(256, 256);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(660, 1045);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "ipTV Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Diagnostics;
+
+
+namespace IPTVCHECKER_C_SHARP
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox2.Text == "") || (richTextBox1.Text == "") || (richTextBox2.Text == ""))
+            {
+                MessageBox.Show("Lütfen Tüm Kutucuları Doldurunuz.", "IpTVChecker", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                textBox1.Text = "";
+                string user = richTextBox1.Text;
+                string[] userlines = user.Split(
+        new[] { "\r\n", "\r", "\n" },
+        StringSplitOptions.None
+    );
+                string pass = richTextBox2.Text;
+                string[] passlines = pass.Split(
+        new[] { "\r\n", "\r", "\n" },
+        StringSplitOptions.None
+    );
+                int satir = richTextBox1.Lines.Count();
+                for (int i = 0; i < satir; i++)
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        HttpResponseMessage response = await client.GetAsync(textBox2.Text + "/get.php?username=" + userlines[i] + "&password=" + passlines[i] + "&type=m3u");
+
+                        if (response.StatusCode.ToString() == "NotFound")
+                        {
+                            textBox1.Text = textBox1.Text + "\r\nNot Working";
+                        }
+                        else
+                        {
+                            string m3u = await new WebClient().DownloadStringTaskAsync(textBox2.Text + "/get.php?username=" + userlines[i] + "&password=" + passlines[i] + "&type=m3u");
+                            if (m3u.Contains("#EXTM3U") == true)
+                            {
+                                textBox1.Text = textBox1.Text + "\r\nWorking; " + userlines[i] + ":" + passlines[i];
+                            }
+                            else
+                            {
+                                textBox1.Text = textBox1.Text + "\r\nNot Working";
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://t.me/quiec");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/quiec");
+        }
+    }
+}
